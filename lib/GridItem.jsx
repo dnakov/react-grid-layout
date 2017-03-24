a0943640c81e7fcfad825b9f321592cbd143847a
Daniel Nakov <daniel.nakov@gmail.com>
2017-03-24 16:46:07 -0400

    Update GridItem.jsx

--- lib/GridItem.jsx
+++ lib/GridItem.jsx
@@ -292,7 +292,7 @@ export default class GridItem extends React.Component {
       // Get new XY
       switch (handlerName) {
         case 'onDragStart': {
-          // ToDo this wont work on nested parents
+          e.preventDefault();
           const parentRect = node.offsetParent.getBoundingClientRect();
           const clientRect = node.getBoundingClientRect();
           newPosition.left = clientRect.left - parentRect.left;
